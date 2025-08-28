2c929e3a351eac6312c3dadfac8f1441db262421
rooha <github@panadey.ru>
2025-08-28 15:17:03 +0300

    вернул компонент когда нет доступа

--- src/App.tsx
+++ src/App.tsx
@@ -122,15 +122,14 @@ export default function App() {
     const checkAuthorization = async () => {
       setIsLoading(true);
       
-      // // Проверяем сохраненного пользователя
-      // const savedUser = localStorage.getItem('currentUser');
-      // if (savedUser) {
-      //   const user = JSON.parse(savedUser);
-      //   setCurrentUser(user);
-      //   setCurrentScreen('home');
-      //   setIsLoading(false);
-      //   return;
-      // }
+      const savedUser = localStorage.getItem('currentUser');
+      if (savedUser) {
+        const user = JSON.parse(savedUser);
+        setCurrentUser(user);
+        setCurrentScreen('home');
+        setIsLoading(false);
+        return;
+      }
 
       // Получаем данные из Telegram
       const telegramUser = getTelegramUser();
